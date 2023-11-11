475504e864086a22bf35368912fd54a8d9e721cc
Pobarul-islam <pobarul3@gmail.com>
2023-11-11 14:43:34 +0600

    client side added

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="bg-black">
+        <h1 className="text-white text-center">Hello world</h1>
+      </div>
+    
+    </>
+  );
+}
+
+export default App;
